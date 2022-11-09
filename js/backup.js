150584ed55449b729a4f5bcf9e9b988f007c1d06
Gyuho Lee <87969508+Gyuho0o@users.noreply.github.com>
2022-11-09 12:41:17 +0900

    Add files via upload

---
+++ js/backup.js
@@ -0,0 +1,54 @@
+var PrimerClub_Under10 = [
+    {
+        id : 0,
+        TeamName_Under10 : '① 119레오',
+        TeamInfo_Under10 : '생명을 구한 방화복을 가방으로 업사이클링',
+        TeamDate_Under10 : '2018년 8월 22일',
+        TeamCEO_Under10 : '이승우',
+        TeamEmail_Under10 : "<a href='mailto:ceo@119reo.com' style='text-decoration: none;'>ceo@119reo.com</a>",
+        TeamWebsite_Under10 : "<a href='https://www.119reo.com/' style='text-decoration: none;' target='_blank'>https://www.119reo.com/</a>",
+        TeamFundingCondition_Under10 : '10억원(post 70~80억원 희망)',
+        TeamPriorvalue_Under10 : '(미팅 시 공개)',
+        TeamPurpose1_Under10 : '제품 개발/생산',
+        TeamPurpose2_Under10 : '인재 확보',
+        TeamPurpose3_Under10 : '온/오프라인 마케팅 진행',
+        TeamSector_Under10 : 'Fashion & Beauty',
+        TeamIR_Under10 : 'https://drive.google.com/file/d/1FPWh8reP_tdgInzPwa0wjR5VcoumCFmf/view?usp=sharing',
+    },
+    {
+        id : 1,
+        TeamName_Under10 : '② 비디에이아이(비디)',
+        TeamInfo_Under10 : 'AI 기반 얼굴편집 어플리케이션',
+        TeamDate_Under10 : '2021년 12월 16일',
+        TeamCEO_Under10 : '김장영',
+        TeamEmail_Under10 : "<a href='mailto:dkel03@bidi-ai.com' style='text-decoration: none;'>dkel03@bidi-ai.com</a>",
+        TeamWebsite_Under10 : "<a href='https://tinyurl.com/3swehd4c' style='text-decoration: none;' target='_blank'>앱스토어</a> / <a href='https://tinyurl.com/526vcua8' style='text-decoration: none;' target='_blank'>플레이스토어</a>",
+        TeamFundingCondition_Under10 : '5억원(post 50억원 희망)',
+        TeamPriorvalue_Under10 : 'post 7.7억원',
+        TeamPurpose1_Under10 : 'Generative AI 개발/연구 팀원 영입',
+        TeamPurpose2_Under10 : '콘텐츠 기획/마케팅 팀원 영입',
+        TeamPurpose3_Under10 : '실험용 GPU 확보',
+        TeamSector_Under10 : 'Contents & Information',
+        TeamIR_Under10 : 'https://tinyurl.com/3uh98tsw',
+    },
+]
+
+
+var PrimerClub_Under100 = [
+    {
+        id : 0,
+        TeamName_Under100 : '⑤ 딥스튜디오(슈퍼카인드)',
+        TeamInfo_Under100 : '가상과 현실이 결합한 아이돌 슈퍼카인드',
+        TeamDate_Under100 : '2017년 8월 3일',
+        TeamCEO_Under100 : '류기현',
+        TeamEmail_Under100 : "<a href='mailto:rukeon@deepstudio.io' style='text-decoration: none;'>rukeon@deepstudio.io</a>",
+        TeamWebsite_Under100 : "<a href='https://superkind.xyz/' style='text-decoration: none;' target='_blank'>https://superkind.xyz/</a>",
+        TeamFundingCondition_Under100 : '100억원(post 1,000억원 희망)',
+        TeamPriorvalue_Under100 : 'post 500억원',
+        TeamPurpose1_Under100 : '3개년 운영자금',
+        TeamPurpose2_Under100 : '콘텐츠 제작비 확보',
+        TeamPurpose3_Under100 : '인재 채용',
+        TeamSector_Under100 : 'Contents & Information',
+        TeamIR_Under100 : 'https://drive.google.com/file/d/1IQOos8B4_hgUxoYYC6c81sKxAzHuv_uI/view?usp=sharing',
+    }
+]
